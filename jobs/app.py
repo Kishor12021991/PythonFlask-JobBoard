b83e3f094a272d8157d8b9c25655f1f0886155c6
Kishor12021991 <Pavan20ja!@#>
2020-04-05 21:01:20 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -5,8 +5,25 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 def open_connection():
     connection = getattr(g,'_connection',None)
-    Connection = None
-    Connection = sqlite3.connect(PATH)
+     if Connection = None:
+         Connection = g._connection = sqlite3.connect(PATH)
+    Connection.row_factory = sqlite3.Row
+    return Connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    Connection = open_connection()
+    cursor=connection.execute(sql,values)
+    if commit=true:
+        results= connection.commit()
+    else:
+        results= cursor.fetchone() if single else curso.fetchall()
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+    curso.close()
+    return results
 @app.route('/')
 @app.route('/jobs')
 def jobs():
